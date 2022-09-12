7c8666118024d78ddeb460aceb2763ab7c1cf894
MuhammadSaadSheikh <msaad9124@gmail.com>
2022-09-12 17:54:33 +0500

    create profile screen

---
+++ src/container/App/Profile/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import {ScrollView, View} from 'react-native';
+
+//GlobalStyle
+import {globalStyle} from '../../../theme';
+
+const Profile = () => {
+  return <ScrollView style={globalStyle.mainContainer}></ScrollView>;
+};
+
+export default Profile;
