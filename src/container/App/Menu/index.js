6d9e80629e57e8abf6035243788b5c0a66415311
MuhammadSaadSheikh <msaad9124@gmail.com>
2022-09-12 17:54:14 +0500

    create menu screen

---
+++ src/container/App/Menu/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import {ScrollView, View} from 'react-native';
+
+//GlobalStyle
+import {globalStyle} from '../../../theme';
+
+const Menu = () => {
+  return <ScrollView style={globalStyle.mainContainer}></ScrollView>;
+};
+
+export default Menu;
